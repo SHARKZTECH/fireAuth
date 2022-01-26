d7de41717827f2f0dc8d40838ba820752e48d1d7
SHARKZTECH <87173929+SHARKZTECH@users.noreply.github.com>
2022-01-26 09:58:07 +0300

    a

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,34 @@
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </Form.Group>
 
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onSubmit={handleLogin}>
         Login
       </Button>
     </Form>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,21 +1,51 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { Form, Button, Container } from "react-bootstrap";
+import { useState } from "react";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    if (!email || !password || !password2) {
+      alert("all fields required");
+    } else if (password !== password2) {
+      alert("passwords don`t match!");
+    } else {
+      console.log(email, password);
+    }
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleRegister}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword2">
           <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Confirm Password" />
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            onChange={(e) => setPassword2(e.target.value)}
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
