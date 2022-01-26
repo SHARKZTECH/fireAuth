e2769d598f1b2a04a4132f496759adc0969c78ec
SHARKZTECH <87173929+SHARKZTECH@users.noreply.github.com>
2022-01-26 09:06:47 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 import "./styles.css";
 
@@ -7,7 +8,8 @@ export default function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </div>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,20 +1,27 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
 export default function Register() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
+    <Container>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Login
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword2">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Confirm Password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </Container>
   );
 }
