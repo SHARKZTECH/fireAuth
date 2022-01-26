d74b7cc84f1bd2fbc559fd39bc0d897626417f83
SHARKZTECH <87173929+SHARKZTECH@users.noreply.github.com>
2022-01-26 09:30:49 +0300

    a

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7_YvgEPCh0AmyZ_vEHf0uCyUZ2Xx2H5w",
+  authDomain: "fireauth-fcb63.firebaseapp.com",
+  projectId: "fireauth-fcb63",
+  storageBucket: "fireauth-fcb63.appspot.com",
+  messagingSenderId: "296873824465",
+  appId: "1:296873824465:web:ffdb392471bc26064e75f0"
+};
+
+export const app = initializeApp(firebaseConfig);
